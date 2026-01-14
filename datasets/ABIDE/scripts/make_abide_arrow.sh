3e3fe51e53abeff18e3d7dca8d0f2b9962a92e59
Connor Lane <connor.lane858@gmail.com>
2026-01-14 23:38:43 +0000

    add streaming to abide and minor updates

---
+++ datasets/ABIDE/scripts/make_abide_arrow.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# all target spaces required by different models
+spaces=(
+    schaefer400
+    schaefer400_tians3
+    flat
+    a424
+    mni
+    mni_cortex
+    schaefer400_tians3_buckner7
+)
+
+# SPACEIDS="0 1 2 3 4 5 6"
+SPACEIDS="3 4 5 6"
+# ROOT="data/fmriprep"
+ROOT="s3://medarc/fmri-fm-eval/ABIDE/fmriprep"
+OUT_ROOT="s3://medarc/fmri-datasets/eval"
+
+log_path="logs/make_abide_arrow.log"
+
+for ii in $SPACEIDS; do
+    space=${spaces[ii]}
+    uv run python scripts/make_abide_arrow.py \
+        --root "${ROOT}" \
+        --out-root "${OUT_ROOT}" \
+        --space "${space}" \
+        2>&1 | tee -a "${log_path}"
+done
